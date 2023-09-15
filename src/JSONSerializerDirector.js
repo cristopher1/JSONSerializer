7e824f13708776e1197822a41ce5b4c1a7e121ab
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-09-15 18:14:30 -0300

    director encargado de llamar a JSONSerializerBuilder.js

---
+++ src/JSONSerializerDirector.js
@@ -0,0 +1,24 @@
+import { JSONSerializerBuilder } from './core/JSONSerializerBuilder'
+
+class JSONSerializerDirector {
+  #builder
+
+  constructor(builder) {
+    this.#builder = builder
+  }
+
+  makeJsonSerializer() {
+    const builder = this.#builder
+
+    builder.createNewJsonSerializer()
+    builder.setReplacerBuilder()
+    builder.setReviverBuilder()
+
+    return builder.getJSONSerializer()
+  }
+}
+
+const jsonSerializerBuilder = new JSONSerializerBuilder()
+const jsonSerializerDirector = new JSONSerializerDirector(jsonSerializerBuilder)
+
+export default jsonSerializerDirector
