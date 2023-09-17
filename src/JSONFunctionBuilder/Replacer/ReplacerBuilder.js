9966123e897d6c341b79f49d092e264562793313
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-09-17 00:49:31 -0300

    se reemplazan los getters de javascript por funciones con el prefijo get, además se usa la función build para crear el replacer

--- src/JSONFunctionBuilder/Replacer/ReplacerBuilder.js
+++ src/JSONFunctionBuilder/Replacer/ReplacerBuilder.js
@@ -6,25 +6,27 @@ export class ReplacerBuilder extends Builder {
     super(serializerHandler, ReplacerBuilderSerializerHandlerError)
   }
 
-  get savedSerializers() {
-    return super.savedSerializers
+  getSavedSerializers() {
+    return super.getSavedSerializers()
   }
 
   addSerializer(serializerType, serializer) {
-    const serializerHandler = super.serializerHandler
+    const serializerHandler = super.getSerializerHandler()
     serializerHandler.addSerializer(serializerType, serializer)
   }
 
-  getReplacer() {
-    const serializerHandler = super.serializerHandler
+  build() {
+    const serializerHandler = super.getSerializerHandler()
     const serializers = serializerHandler.serializers
-    const getSerializerType = (data) => {
-      const type = typeof data
-      return type === 'object' ? data.constructor.name.toLowerCase() : type
+    const getSerializerType = (dataType) => {
+      const type = typeof dataType
+      return type === 'object' ? dataType.constructor.name.toLowerCase() : type
     }
+
     return function replacer(key, value) {
       const unserializedData = this[key]
-      const serializerType = getSerializerType(unserializedData)
+      const dataType = unserializedData.__typeof__
+      const serializerType = getSerializerType(dataType)
       const serializer = serializers[serializerType]
       if (serializer) {
         return serializer.serialize(unserializedData)
