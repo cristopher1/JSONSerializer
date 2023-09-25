ee11112e993b1f31e771ad3b37e45f1d603356bf
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-09-25 14:02:36 -0300

    ValidatorHandler se encarga de administrar las validaciones

---
+++ src/ValidatorHandler/ValidatorHandler.js
@@ -0,0 +1,14 @@
+export class ValidatorHandler {
+  #validators
+
+  constructor(validators) {
+    this.#validators = validators
+  }
+
+  validate(data) {
+    const validators = this.#validators
+    for (const validator of validators) {
+      validator.validate(data)
+    }
+  }
+}
