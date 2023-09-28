6410f420e12320cb2d71cb3ce2e6c6d14189c84b
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-09-28 18:02:53 -0300

    tests para ValidatorError, se prueba las propiedades name y message

---
+++ __tests__/Validators/ValidatorError.test.js
@@ -0,0 +1,37 @@
+import { ValidatorError } from '../../src/Validator/Error'
+import { faker } from '../helpers'
+
+const filePath = 'src/Validator/Error.js'
+
+describe(`class ValidatorError (${filePath})`, () => {
+  describe('(property) name', () => {
+    it('Should return a string with the name passed in the constructor', () => {
+      // Arrange
+      const name = faker.string.nanoid()
+      const message = faker.string.sample()
+      const validatorError = new ValidatorError(name, message)
+      const expected = name
+
+      // Act
+      const result = validatorError.name
+
+      // Assert
+      expect(result).toBe(expected)
+    })
+  })
+  describe('(property) message', () => {
+    it('Should return a string with the message passed in the constructor', () => {
+      // Arrange
+      const name = faker.string.nanoid()
+      const message = faker.string.sample()
+      const validatorError = new ValidatorError(name, message)
+      const expected = message
+
+      // Act
+      const result = validatorError.message
+
+      // Assert
+      expect(result).toBe(expected)
+    })
+  })
+})
