347498e91ea017785497fd11927c84b1e3b5e690
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-09-29 00:39:17 -0300

    se agregan tests para serialize y parse

--- __tests__/JsonSerializer.test.js
+++ __tests__/JsonSerializer.test.js
@@ -1,8 +1,9 @@
-import { getSerializer } from './helpers'
+import { faker, getSerializer } from './helpers'
 import { ValidatorError } from '../src/Validator/Error'
 import { JsonSerializer } from '../src/core/JsonSerializer'
 import { buildJsonSerializer } from '../src'
 
+/* eslint-disable no-eval */
 const filePath = 'src/index.js'
 
 describe(`export function buildJsonSerializer (${filePath})`, () => {
@@ -190,6 +191,198 @@ describe(`export function buildJsonSerializer (${filePath})`, () => {
 
         // Assert
         expect(result).toEqual(expected)
+      })
+    })
+    describe('(method) serialize', () => {
+      describe('When there are not serializers', () => {
+        it('Should return a serialized data using default JSON.stringify', () => {
+          // Arrange
+          const unserializedData = faker.string.sample()
+          const expected = `"${unserializedData}"`
+
+          // Act
+          const result = jsonSerializer.serialize(unserializedData)
+
+          // Assert
+          expect(result).toBe(expected)
+        })
+      })
+      describe('When there are serializers', () => {
+        it('Should return a serialized data when there is a serializer for that data and its typeof is different from object', () => {
+          // Arrange
+          const unserializedData = faker.number.bigInt()
+          const serializer = getSerializer(
+            () => 'bigint',
+            (unserializerData) => ({ value: unserializerData.toString() }),
+            (serializedData) => {
+              const { value } = serializedData
+              return BigInt(value)
+            },
+          )
+          const expected = `{"__typeof__":"bigint","value":"${unserializedData.toString()}"}`
+
+          jsonSerializer.addSerializerAndRefreshJsonSerializer(serializer)
+
+          // Act
+          const result = jsonSerializer.serialize(unserializedData)
+
+          // Assert
+          expect(result).toBe(expected)
+        })
+        it('Should return a serialized data when there is a serializer for that data and its typeof is object', () => {
+          // Arrange
+          class MyObject {
+            constructor(data1) {
+              this.data1 = data1
+            }
+
+            toString() {
+              return `({ data1: ${this.data1}})`
+            }
+          }
+          const unserializedData = new MyObject('string')
+          const serializer = getSerializer(
+            () => 'MyObject',
+            (unserializerData) => ({ value: unserializerData.toString() }),
+            (serializedData) => {
+              const { value } = serializedData
+              const parameters = Object.values(eval(value))
+              return new MyObject(...parameters)
+            },
+          )
+          const expected = `{"__typeof__":"MyObject","value":"${unserializedData.toString()}"}`
+
+          jsonSerializer.addSerializerAndRefreshJsonSerializer(serializer)
+
+          // Act
+          const result = jsonSerializer.serialize(unserializedData)
+
+          // Assert
+          expect(result).toBe(expected)
+        })
+        it('Should not return a serialized data when there is not a serializer for that data and JSON.stringify can not process it', () => {
+          // Arrange
+          const unserializedData = () => {}
+          const serializer = getSerializer(
+            () => 'bigint',
+            (unserializerData) => ({ value: unserializerData.toString() }),
+            (serializedData) => {
+              const { value } = serializedData
+              return BigInt(value)
+            },
+          )
+          const expected = undefined
+
+          jsonSerializer.addSerializerAndRefreshJsonSerializer(serializer)
+
+          // Act
+          const result = jsonSerializer.serialize(unserializedData)
+
+          // Assert
+          expect(result).toBe(expected)
+        })
+      })
+      describe('(method) parse', () => {
+        describe('When there are not serializers', () => {
+          it('Should return unserialized data using default JSON.parse', () => {
+            // Arrange
+            const unserializedData = faker.string.sample()
+            const serializedData = `"${unserializedData}"`
+            const expected = unserializedData
+
+            // Act
+            const result = jsonSerializer.parse(serializedData)
+
+            // Assert
+            expect(result).toBe(expected)
+          })
+        })
+        describe('When there are serializers', () => {
+          it('Should return unserialized data when there is a serializer for that data and its typeof is different from object', () => {
+            // Arrange
+            const unserializedData = faker.number.bigInt()
+            const serializer = getSerializer(
+              () => 'bigint',
+              (unserializerData) => ({ value: unserializerData.toString() }),
+              (serializedData) => {
+                const { value } = serializedData
+                return BigInt(value)
+              },
+            )
+
+            jsonSerializer.addSerializerAndRefreshJsonSerializer(serializer)
+
+            const serializedData = jsonSerializer.serialize(unserializedData)
+            const expected = unserializedData
+
+            // Act
+            const result = jsonSerializer.parse(serializedData)
+
+            // Assert
+            expect(result).toBe(expected)
+          })
+          it('Should return unserialized data when there is a serializer for that data and its typeof is object', () => {
+            // Arrange
+            class NewObject {
+              #data3
+
+              constructor(data1, data2, data3) {
+                this.data1 = data1
+                this.data2 = data2
+                this.#data3 = data3
+              }
+
+              getData1() {
+                return this.data1
+              }
+
+              getData2() {
+                return this.data2
+              }
+
+              getData3() {
+                return this.#data3
+              }
+
+              toString() {
+                return `(
+                  { 
+                    data1: "${this.data1}",
+                    data2: [${this.data2}],
+                    data3: "${this.#data3}"
+                  }
+                )`
+              }
+            }
+            const unserializedData = new NewObject(
+              'string',
+              [1, 2, 3],
+              'lalalalal',
+            )
+            const serializer = getSerializer(
+              () => 'NewObject',
+              (unserializerData) => ({ value: unserializerData.toString() }),
+              (serializedData) => {
+                const { value } = serializedData
+                const parameters = Object.values(eval(value))
+                return new NewObject(...parameters)
+              },
+            )
+
+            jsonSerializer.addSerializerAndRefreshJsonSerializer(serializer)
+
+            const serializedData = jsonSerializer.serialize(unserializedData)
+            const expected = unserializedData
+
+            // Act
+            const result = jsonSerializer.parse(serializedData)
+
+            // Assert
+            expect(result.getData1()).toBe(expected.getData1())
+            expect(result.getData2()).toEqual(expected.getData2())
+            expect(result.getData3()).toBe(expected.getData3())
+          })
+        })
       })
     })
   })
