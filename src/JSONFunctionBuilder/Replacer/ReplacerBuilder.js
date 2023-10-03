80663283d58d2983846237366f999fcddb334461
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-10-03 01:00:05 -0300

    uso de jsdoc

--- src/JSONFunctionBuilder/Replacer/ReplacerBuilder.js
+++ src/JSONFunctionBuilder/Replacer/ReplacerBuilder.js
@@ -5,10 +5,25 @@ export class ReplacerBuilder {
     this.#serializers = {}
   }
 
+  /**
+   * Sets serializers. When the build method is called, the serializers are
+   * added to the replacer function.
+   *
+   * @param {object} serializers An object that contains serializers added to
+   *   JsonSerializer object. The keys are obtained from
+   *   serializer.getSerializerType method and the values are the Serializer
+   *   objects.
+   */
   setSerializers(serializers) {
     this.#serializers = serializers
   }
 
+  /**
+   * Builds a replacer function using builder pattern.
+   *
+   * @returns {(key: string, value: any) => any} Replacer function used by
+   *   JSON.stringify method.
+   */
   build() {
     const serializers = this.#serializers
     const getSerializerType = (unserializedData) => {
--- src/JSONFunctionBuilder/Reviver/ReviverBuilder.js
+++ src/JSONFunctionBuilder/Reviver/ReviverBuilder.js
@@ -5,10 +5,25 @@ export class ReviverBuilder {
     this.#serializers = {}
   }
 
+  /**
+   * Sets serializers. When the build method is called, the serializers are
+   * added to the reviver function.
+   *
+   * @param {object} serializers An object that contains serializers added to
+   *   JsonSerializer object. The keys are obtained from
+   *   serializer.getSerializerType method and the values are the Serializer
+   *   objects.
+   */
   setSerializers(serializers) {
     this.#serializers = serializers
   }
 
+  /**
+   * Builds a reviver function using builder pattern.
+   *
+   * @returns {(key: string, value: any) => any} Reviver function used by
+   *   JSON.parse method.
+   */
   build() {
     const serializers = this.#serializers
 
