49d223a2963e726028c228d29cc920817fd54c8f
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-10-03 01:42:35 -0300

    uso de jsdoc en el constructor y uso de Error en jsdoc

--- src/ValidatorHandler/ValidatorHandler.js
+++ src/ValidatorHandler/ValidatorHandler.js
@@ -4,7 +4,9 @@ export class ValidatorHandler {
   #validators
 
   /**
-   * @param {Validator[]} validators Array that contains validator objects used
+   * Object used to manage the validation through Validator objects.
+   *
+   * @param {Validator[]} validators Array that contains Validator objects used
    *   to validate data.
    */
   constructor(validators) {
