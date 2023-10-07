013444b49f9c60c803ce765d1188468939ac13df
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-10-07 17:58:32 -0300

    uso de factory para crear instancias de JsonSerializer

--- src/index.js
+++ src/index.js
@@ -7,31 +7,32 @@ import { ReviverBuilder } from './functionsBuilder/reviver/ReviverBuilder'
 import { SerializerHandler } from './serializers/handler/SerializerHandler'
 import { JsonSerializer } from './core/JSONSerializer'
 
-/**
- * Builds a JsonSerializer object using the builder pattern. Every call to this
- * function generates a new instance of JsonSerializer.
- *
- * @returns {JsonSerializer} A new instance of JsonSerializer.
- */
-export function buildJsonSerializer() {
-  const serializerValidators = [
-    new PropertyContainsValidTypeValidator(
-      new ContainsPropertyValidator('getSerializerType'),
-      new IsTypeValidator('function'),
-    ),
-    new PropertyContainsValidTypeValidator(
-      new ContainsPropertyValidator('serialize'),
-      new IsTypeValidator('function'),
-    ),
-    new PropertyContainsValidTypeValidator(
-      new ContainsPropertyValidator('parse'),
-      new IsTypeValidator('function'),
-    ),
-  ]
+export class JsonSerializerFactory {
+  /**
+   * Create a new instance of JsonSerializer
+   *
+   * @returns {JsonSerializer} A new instance of JsonSerializer
+   */
+  static createJsonSerializer() {
+    const serializerValidators = [
+      new PropertyContainsValidTypeValidator(
+        new ContainsPropertyValidator('getSerializerType'),
+        new IsTypeValidator('function'),
+      ),
+      new PropertyContainsValidTypeValidator(
+        new ContainsPropertyValidator('serialize'),
+        new IsTypeValidator('function'),
+      ),
+      new PropertyContainsValidTypeValidator(
+        new ContainsPropertyValidator('parse'),
+        new IsTypeValidator('function'),
+      ),
+    ]
 
-  return new JsonSerializer(
-    new ReplacerBuilder(),
-    new ReviverBuilder(),
-    new SerializerHandler(new ValidatorHandler(serializerValidators)),
-  )
+    return new JsonSerializer(
+      new ReplacerBuilder(),
+      new ReviverBuilder(),
+      new SerializerHandler(new ValidatorHandler(serializerValidators)),
+    )
+  }
 }
